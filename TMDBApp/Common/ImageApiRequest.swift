c23539c956b6c19549aa5ddd3f2ee89ad0b4fa30
Tomasz Paluch <paluch.t@wp.pl>
2025-01-12 16:42:34 +0100

    Add image displaying function

---
+++ TMDBApp/Common/ImageApiRequest.swift
@@ -0,0 +1,8 @@
+//
+//  ImageApiRequest.swift
+//  TMDBApp
+//
+//  Created by Tomasz Paluch on 12/01/2025.
+//
+
+import Foundation
---
+++ TMDBApp/Common/ImagesApiRequestable.swift
@@ -0,0 +1,8 @@
+//
+//  ImagesApiRequestable.swift
+//  TMDBApp
+//
+//  Created by Tomasz Paluch on 12/01/2025.
+//
+
+import Foundation
